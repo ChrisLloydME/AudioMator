cd2c267592e892b6842cbefab378e8d7745b7ca2
Chris Lloyd <192208532+ChrisLloydME@users.noreply.github.com>
2025-11-22 21:08:49 +0800

    chore: import initial project

---
+++ AudioMator/AudioMatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioMatorApp.swift
+//  AudioMator
+//
+//  Created by Christopher Lloyd on 2025.11.22.
+//
+
+import SwiftUI
+
+@main
+struct AudioMatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioMator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioMator
+//
+//  Created by Christopher Lloyd on 2025.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

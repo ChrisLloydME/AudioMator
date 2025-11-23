89bb1081f007b4a5b552485964923744cd455e70
Chris Lloyd <192208532+ChrisLloydME@users.noreply.github.com>
2025-11-23 17:16:25 +0800

    chore: update default window size.

--- AudioMator/AudioMatorApp.swift
+++ AudioMator/AudioMatorApp.swift
@@ -9,6 +9,20 @@ import SwiftUI
 import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private let hasLaunchedKey = "hasLaunchedBefore"
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let launchedBefore = UserDefaults.standard.bool(forKey: hasLaunchedKey)
+
+        if !launchedBefore {
+            if let window = NSApplication.shared.windows.first {
+                window.setContentSize(NSSize(width: 900, height: 600))
+                window.center()
+            }
+            UserDefaults.standard.set(true, forKey: hasLaunchedKey)
+        }
+    }
+
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         true
     }
@@ -20,6 +34,8 @@ struct AudioMatorApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(minWidth: 900, minHeight: 600)
         }
+        .defaultSize(width: 900, height: 600)
     }
 }

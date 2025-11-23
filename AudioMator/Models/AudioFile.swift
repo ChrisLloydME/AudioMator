c85095145dce8f7c0a1d004818cb52990e531f56
Chris Lloyd <192208532+ChrisLloydME@users.noreply.github.com>
2025-11-23 16:51:40 +0800

    feat: use AVFoundation to display album artwork and tech specs.

--- AudioMator/Models/AudioFile.swift
+++ AudioMator/Models/AudioFile.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 import SFBAudioEngine
+import AVFoundation
+import CoreMedia
+import AppKit
 
 struct AudioFile: Identifiable {
     let id = UUID()
@@ -24,6 +27,16 @@ struct AudioFile: Identifiable {
     let disc: Int
     let discTotal: Int
     let year: String
+
+    // MARK: – Technical
+    let duration: Double
+    let bitrate: Int
+    let sampleRate: Double
+    let channels: Int
+    let format: String
+
+    // MARK: – Artwork
+    let artwork: NSImage?
 
     init(url: URL) throws {
         self.url = url
@@ -45,5 +58,65 @@ struct AudioFile: Identifiable {
         self.discTotal = meta.discTotal ?? 0
 
         self.year = meta.releaseDate ?? ""
+
+        // MARK: – Technical info via AVFoundation
+        let asset = AVURLAsset(url: url)
+
+        var durationSeconds: Double = 0
+        var bitrateKbps: Int = 0
+        var sampleRateHz: Double = 0
+        var channelsCount: Int = 0
+        var formatName: String = ""
+
+        // Duration
+        durationSeconds = CMTimeGetSeconds(asset.duration)
+
+        // Use first audio track for technical details
+        if let track = asset.tracks(withMediaType: .audio).first {
+            // estimatedDataRate is in bits per second
+            bitrateKbps = Int(track.estimatedDataRate / 1000)
+
+            if let formatDescAny = track.formatDescriptions.first,
+               CFGetTypeID(formatDescAny as CFTypeRef) == CMFormatDescriptionGetTypeID(),
+               let asbdPtr = CMAudioFormatDescriptionGetStreamBasicDescription(formatDescAny as! CMAudioFormatDescription) {
+                let asbd = asbdPtr.pointee
+                sampleRateHz = asbd.mSampleRate
+                channelsCount = Int(asbd.mChannelsPerFrame)
+
+                switch asbd.mFormatID {
+                case kAudioFormatMPEGLayer3:
+                    formatName = "MP3"
+                case kAudioFormatMPEG4AAC:
+                    formatName = "AAC"
+                default:
+                    formatName = "Audio"
+                }
+            }
+        }
+
+        self.duration = durationSeconds
+        self.bitrate = bitrateKbps
+        self.sampleRate = sampleRateHz
+        self.channels = channelsCount
+        self.format = formatName
+
+        // MARK: – Artwork via AVFoundation
+        var artworkImage: NSImage? = nil
+
+        let commonArtwork = AVMetadataItem.metadataItems(
+            from: asset.commonMetadata,
+            withKey: AVMetadataKey.commonKeyArtwork,
+            keySpace: .common
+        )
+
+        if let item = commonArtwork.first {
+            if let data = item.dataValue {
+                artworkImage = NSImage(data: data)
+            } else if let value = item.value as? Data {
+                artworkImage = NSImage(data: value)
+            }
+        }
+
+        self.artwork = artworkImage
     }
 }
